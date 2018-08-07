3cda103c5732daa8c7015c7b4ce370b0806045f1
yunglleung1 <yungl.leung1@gmail.com>
2018-08-07 08:21:07 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  # Gets Page newteam.erb w/ form: Browser URL
+  # renders the form to create
+  get '/newteam' do
+    erb :newteam
+  end
 
+  # Posts via Form from newteam.erb: method/action
+  # actually creates and then should REDIRECT to show page for team
+  post '/team' do
+    @team = params
+    erb :team
+  end
+
+  # Gets posted data
+  # get '/team' do
+  #   erb :team
+  # end
 
 end
